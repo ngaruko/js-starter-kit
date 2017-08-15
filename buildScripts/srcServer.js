a658dc23c802a00bc193a28406dac46b9ffea790
ngaruko <ngarukob@yahoo.com>
2017-08-16 10:25:15 +1200

    bundling with webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-	const port = 3000,
-	app = express();
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+const port = 3000,
+	app = express(),
+	compiler = webpack(config);
 	
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: false,
+	publicPath:config.output.publicPath
+}));
+
 // GET method route
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -18,7 +27,7 @@ app.listen(port, function(err) {
 	if (err) {
 		console.log(err);
 	} else {
-		open('http://localhost:'+ port);
+		open('http://localhost:' + port);
 	}
-	
+
 });

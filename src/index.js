8768860a4693c9e4607c010d83c4f5b3751813af
ngaruko <ngarukob@yahoo.com>
2017-08-16 16:14:20 +1200

    Cleaned code

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ let usersBody = "";
 		<td>${user.id}</td>
 		<td>${user.firstName}</td>
 		<td>${user.lastName}</td>
-		<td>${user.email}</td>
-		<td>Test table</td>
-		</tr>`;
+		<td>${user.email}</td> </tr>`;
 	});
-	
 	global.document.getElementById('users').innerHTML = usersBody;
 	
 });

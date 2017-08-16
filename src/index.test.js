3a0c21ae56586032eb1ff5d5dd9c6e5a3e05cd47
ngaruko <ngarukob@yahoo.com>
2017-08-16 13:46:26 +1200

    testing with jsdom

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,21 @@
 import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
 	it('should pass', () => {
-		expect(2===3 - 1).to.equal(true);
+		expect(2 === 3 - 1).to.equal(true);
+	});
+});
+
+describe('index.html', () => {
+	it('should say Invincible', (done) => {//note; since we are using async calls, we need the done, otherwsie, doesnt work
+		const index = fs.readFileSync('./src/index.html', "utf-8");
+		jsdom.env(index, (err, window) => {
+			const header = window.document.getElementsByTagName('h1')[0];
+			expect(header.innerHTML).to.equal('We are invincible');
+			done();
+			window.close();
+		});
 	});
 });

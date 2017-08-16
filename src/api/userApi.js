e5d9277c394ca5e8cff4c77ff1db745d67f12353
ngaruko <ngarukob@yahoo.com>
2017-08-16 21:39:10 +1200

    http mock

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,14 +1,27 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
 /* 	eslint-disable no-console	*/
-export function getUsers() {
-	return get('users');
-}
+const baseUrl = getBaseUrl();
 function get(url) {
-	return fetch(url).then(onSuccess, onError);
+	return fetch(baseUrl + url).then(onSuccess, onError);
 }
 function onSuccess(response) {
 	return response.json();
 }
 function onError(error) {
 	return console.log(error);
+}
+
+function del(url) {
+	const request = new Request(baseUrl + url, {
+		method: 'DELETE'
+	});
+	return fetch(request).then(onSuccess, onError);
+}
+
+export function getUsers() {
+	return get('users');
+}
+export function deleteUser(id) {
+	return del(`users/${id}`);
 }
